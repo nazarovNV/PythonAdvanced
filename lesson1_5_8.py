01ed6f37fff8d4c10aae098f51bf9ff6a245693b
nazarov_nv <nikolaynazarovvv216@gmail.com>
2022-05-16 21:08:55 +0300

    Second commit

--- lesson1_5_8.py
+++ lesson1_5_8.py
@@ -1,16 +1,11 @@
 class MoneyBox:
     def __init__(self, capacity):
         self.capacity = capacity
-        # конструктор с аргументом – вместимость копилки
+        self.count = 0
 
-    #def can_add(self, v):
-
-        # True, если можно добавить v монет, False иначе
+    def can_add(self, v):
+        return self.count + v <= self.capacity
 
     def add(self, v):
-        self.v += v
-
-        # положить v монет в копилку
-
-firstbox = MoneyBox(10)
-print(f'Вместимость равна {firstbox.capacity}')
+        if self.can_add(v):
+            self.count += v
